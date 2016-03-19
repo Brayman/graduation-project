fddfd173fcd0dd33cf90987b76d57869434f7f90
Artsiom Rakitski <Artsiom_Rakitski@epam.com>
2016-03-19 16:15:52 +0300

    подключение react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import App from './components/HomePage';
+import Profile from './components/Profile';
+import Menu from './components/HeaderMenu';
 import store from './state'
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <Router history={browserHistory}>
+            <Route path="/" component={Menu}>
+                <IndexRoute component={App}/>
+                <Route path="profile/:user/:id" component={Profile}/>
+            </Route>
+
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
