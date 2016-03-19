18c00c0382af3cb94f7a190d7f4b77f2fccd78cd
Artsiom Rakitski <Artsiom_Rakitski@epam.com>
2016-03-19 16:13:25 +0300

    импортирование линков. Картинка теперь ссылка на роут профиля

--- src/components/ListInfo.js
+++ src/components/ListInfo.js
@@ -1,13 +1,15 @@
 var React = require('react');
-
+import {Link} from 'react-router'
 var ListInfo = React.createClass({
 
     render: function() {
         return (
           <div className="Block">
-            <img className="MediumImage Right Hover"
-                 src="https://static-cdn.jtvnw.net/jtv_user_pictures/c_a_k_e-profile_image-b25ae37f0296d0f1-300x300.jpeg"
-                 alt=""/>
+            <Link to={"/profile/"+this.props.data.username+"/"+this.props.data.id}>
+                <img className="MediumImage Right Hover"
+                     src="https://static-cdn.jtvnw.net/jtv_user_pictures/c_a_k_e-profile_image-b25ae37f0296d0f1-300x300.jpeg"
+                     alt=""/>
+            </Link>
             <div className="RowAll">
               <div>
                 <h4 className="PostText">
