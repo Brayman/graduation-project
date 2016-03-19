a5998c7c65746d5d60564565ea948c3f8fe1c4cb
Artsiom Rakitski <Artsiom_Rakitski@epam.com>
2016-03-19 16:08:53 +0300

    сделал её основной, всероуты грузятся как её дети

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -1,36 +1,43 @@
 var React = require('react');
-
+import {Link} from 'react-router'
 var HeaderMenu = React.createClass({
 render: function() {
 return (
-  <div className="Nav">
-    <div className="Menu">
-        <div className="Hover"><img className="LolImage"
-          src="https://lh6.googleusercontent.com/-IzoUthdKiYA/Vull4PO5fXI/AAAAAAAAAA4/zDoB0OTDEg4sO06rl02kuvvFuENEq9_-Q/s256-p/logo.png"
-          alt="Logo"/>
-        </div>
-        <div className="MenuRight">
-          <div className="Hover">
-            <img className="LolImage"
-            src="https://lh3.googleusercontent.com/-_s9ls0NuuBs/Vull4W1SYrI/AAAAAAAAABA/6wERcNgCmZoQbRWEZ6OUSkipE970gMbaw/w126-h125-p/search.png"
-            alt="Searsh"/>
-          </div>
-        <div className="Hover">
-          <div className="Sms"></div>
-          <div className="MenuTextSms">
-            1
-          </div>
-        </div>
-        <div className="Hover">
+  <div>
+      <div className="Nav">
+          <div className="Menu">
+              <div className="Hover">
+                  <Link to="/">
+                      <img className="LolImage"
+                           src="https://lh6.googleusercontent.com/-IzoUthdKiYA/Vull4PO5fXI/AAAAAAAAAA4/zDoB0OTDEg4sO06rl02kuvvFuENEq9_-Q/s256-p/logo.png"
+                           alt="Logo"
+                      />
+                  </Link>
+              </div>
+              <div className="MenuRight">
+                  <div className="Hover">
+                      <img className="LolImage"
+                           src="https://lh3.googleusercontent.com/-_s9ls0NuuBs/Vull4W1SYrI/AAAAAAAAABA/6wERcNgCmZoQbRWEZ6OUSkipE970gMbaw/w126-h125-p/search.png"
+                           alt="Searsh"/>
+                  </div>
+                  <div className="Hover">
+                      <div className="Sms"></div>
+                      <div className="MenuTextSms">
+                          1
+                      </div>
+                  </div>
+                  <div className="Hover">
           <span className="MenuTextName">
             name
           </span>
-          <img className="LolImage Square Circle"
-          src="https://lh3.googleusercontent.com/-2ISHLNjuIts/Vull4KcLT-I/AAAAAAAAAA8/iC8vzN3ycTU8McfvZzA5iZ700Sezpddiw/w595-h334-no/Non.png"
-          alt="Ava"/>
-        </div>
+                      <img className="LolImage Square Circle"
+                           src="https://lh3.googleusercontent.com/-2ISHLNjuIts/Vull4KcLT-I/AAAAAAAAAA8/iC8vzN3ycTU8McfvZzA5iZ700Sezpddiw/w595-h334-no/Non.png"
+                           alt="Ava"/>
+                  </div>
+              </div>
+          </div>
       </div>
-    </div>
+      {this.props.children}
   </div>
     );
   }
