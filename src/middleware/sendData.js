e7f8c9c7cc734f19f017db0db4d825ec6bfc0aae
Artsiom Rakitski <Artsiom_Rakitski@epam.com>
2016-04-08 16:13:52 +0300

    refactor code

--- src/middleware/sendData.js
+++ src/middleware/sendData.js
@@ -1,18 +1,24 @@
-const  sendData = store => next => action =>{
-if (action.type !== 'REQUEST'){
-    return next(action)
-}
-const [startAction, successAction, failAction]= action.actions;
-store.dispatch({
-    type: startAction
-});
-action.promise.then((data)=>store.dispatch({
-    type: successAction,
-    data
-}),(error)=> store.dispatch({
-    type: failAction,
-    error: error.message
-}))
+const sendData = store => next => action =>{
+    if (action.type !== 'REQUEST') {
+        return next(action);
+    }
+    const [startAction, successAction, failAction] = action.actions;
+    store.dispatch({
+        type: startAction
+    });
+
+    action.promise.then(
+        (data)=>{
+            store.dispatch({
+                type: successAction,
+                data
+            });
+        },
+        (error)=> store.dispatch({
+            type: failAction,
+            error: error
+        })
+    );
 };
 export default sendData;
 /**
