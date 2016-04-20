b9007104ff50ef40c57d2b018df6e2065f6113aa
xolod <Xolod_96@mail.ru>
2016-04-21 00:50:54 +0300

    settings change

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ ReactDOM.render(
                 <Route path="about" component={About}/>
                 <Route path=":type/:user" component={Profile}/>
             </Route>
-
+            
         </Router>
     </Provider>,
     document.getElementById('root')
