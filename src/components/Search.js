303d68ac1a2d457a83eafa12ec52c032f53639b7
Владислав Синица < >
2016-04-27 14:14:12 +0300

    Search rage eah

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,17 +5,50 @@ import {connect} from 'react-redux';
 const Search = React.createClass({
     render() {
         return (
-            <div className="RowFlex">
-                <div>
-                  <input type="text" ref='text'/>
-                  <button onClick={() => this.props.dispatch(search(this.refs.text.value))}>
-                    <i className="search icon"/>
-                  </button>
+            <div className="RowFlex JCC">
+                <div className="item3p BorderRight">
+                  <h2 className="BorderBottom h2">Критерии поиска</h2>
+                  <h4 className="h4SC">Теги</h4>
+                  <div className="SearchChet">
+                    <div className="toggle">
+                      <input type="checkbox" id="1"/>
+                      <label htmlFor="1"></label>
+                    </div><span className="SD">Ремонт</span>
+                  </div>
+                  <div className="SearchChet">
+                    <div className="toggle">
+                      <input type="checkbox" id="2"/>
+                      <label htmlFor="2"></label>
+                    </div><span className="SD">Питание</span>
+                  </div>
+                  <div className="SearchChet">
+                    <div className="toggle">
+                      <input type="checkbox" id="3"/>
+                      <label htmlFor="3"></label>
+                    </div><span className="SD">Развлечения</span>
+                  </div>
+                  <div className="SearchChet">
+                    <div className="toggle">
+                      <input type="checkbox" id="4"/>
+                      <label htmlFor="4"></label>
+                    </div><span className="SD">Дизайн</span>
+                  </div>
+                  <div className="SearchChet BorderBottom">
+                    <div className="toggle">
+                      <input type="checkbox" id="5"/>
+                      <label htmlFor="5"></label>
+                    </div><span className="SD">Продукты</span>
+                  </div>
+                  <h4 className="h4S">Местоположение</h4>
+                  <input className="SI" type="text"/>
                 </div>
-                <div>
-                  <button onClick={() => this.props.dispatch(search('repair'))}>ремонт</button>
-                  <button>авто</button>
-                  <button>прицепы</button>
+                <div className="item2p">
+                  <div className="BorderBottom">
+                    <input className="InputSearch" type="text" ref='text'/>
+                    <button className="BtnSearch" onClick={() => this.props.dispatch(search(this.refs.text.value))}>
+                      <i className="search icon"/>
+                    </button>
+                  </div>
                 </div>
             </div>
         );
